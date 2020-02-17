2c2f6642195dcb13686fb664449936c4b30de3e0
Moshiur <rmoshiur705@gmail.com>
2020-02-17 14:55:14 +0600

    added loop functionality

--- app.js
+++ app.js
@@ -6,3 +6,42 @@ app.controller('Headerctrl',function($scope){
         title: 'Hello Angular Js QuickStart'
     }
 });
+
+app.controller('BookListCtrl', function ($scope) {
+    $scope.books = [
+        {
+            "id": "1",
+            "name": "algorithm",
+            "year": "2018"
+          },
+          {
+            "id": "2",
+            "name": "angularjs",
+            "year": "2018"
+          },
+          {
+            "id": "3",
+            "name": "java",
+            "year": "2018"
+          },
+          {
+            "id": "4",
+            "name": "javascript",
+            "year": "2018"
+          },
+          {
+            "id": "5",
+            "name": "typescript",
+            "year": "2018"
+          }
+          ,{
+            "id": "6",
+            "name": "React",
+            "year": "2018"
+          }
+    ]
+
+    $scope.addToCart = function (book) {
+        console.log(book.id);
+    }
+})
